295eadda23ee48dc2f849cc481b2a878113ef04a
IMSneverDown <moussasidikouissaka019@gmail.com>
2025-03-14 18:26:11 +0000

    prise en compte de la nouvelle instance

--- connexion.js
+++ connexion.js
@@ -1,10 +1,10 @@
 const neo4j = require('neo4j-driver');
 
 const driver = neo4j.driver(
-  process.env.NEO4J_URI || 'neo4j+s://513c02fb.databases.neo4j.io',
+  process.env.NEO4J_URI || 'neo4j+s://dd200c03.databases.neo4j.io',
   neo4j.auth.basic(
     process.env.NEO4J_USERNAME || 'neo4j',
-    process.env.NEO4J_PASSWORD || 't1dLqE3Y8wwKeuYE1pxif1CxUhGXvPyhOpXdHqHvJtM'
+    process.env.NEO4J_PASSWORD || '24C06gLS9P7Jn8vOxd9OMRAsq-YlPBRUJrjEmuzo_kY'
   )
 );
 
